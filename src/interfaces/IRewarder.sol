564f73c19a1aaed662862fbf95ee05669b6fcb35
Mathieu <85969303+Mathieu-Be@users.noreply.github.com>
2023-07-04 22:02:56 +0200

    fix audit extra issue (#6)

--- src/interfaces/IRewarder.sol
+++ src/interfaces/IRewarder.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.10;
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 interface IRewarder {
-    function onJoeReward(address user, uint256 newLpAmount) external;
+    function onJoeReward(address user, uint256 newLpAmount, uint256 aptSupply) external;
 
     function pendingTokens(address user) external view returns (uint256 pending);
 
